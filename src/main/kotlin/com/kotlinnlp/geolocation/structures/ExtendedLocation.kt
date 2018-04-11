a4138a18a692e746eae886385e4d816fa7200168
KotlinNLP Authors <github@kotlinnlp.com>
2018-04-11 17:37:23 +0200

    Implement method boostByParent()

--- src/main/kotlin/com/kotlinnlp/geolocation/structures/ExtendedLocation.kt
+++ src/main/kotlin/com/kotlinnlp/geolocation/structures/ExtendedLocation.kt
@@ -7,6 +7,8 @@
 
 package com.kotlinnlp.geolocation.structures
 
+import java.lang.Double.max
+
 /**
  * A structure that extends a location with adding properties.
  *
@@ -14,14 +16,12 @@ package com.kotlinnlp.geolocation.structures
  * @property parents the list of the [location] parents, in the same order of its 'parentIds' property
  * @property entities the list of entities from which this location originated
  * @property initScore the initial score of the location
- * @property boost the score boosts structure
  */
 data class ExtendedLocation(
   val location: Location,
   val parents: List<Location>,
   val entities: List<CandidateEntity>,
-  val initScore: Double,
-  val boost: Boost = Boost()
+  val initScore: Double
 ) {
 
   /**
@@ -31,7 +31,7 @@ data class ExtendedLocation(
    * @property parents a map of score boosts associated by parent entity
    * @property brothers a map of score boosts associated by brother entity
    */
-  data class Boost(
+  private data class Boost(
     var children: MutableMap<String, Double> = mutableMapOf(),
     var parents: MutableMap<String, Double> = mutableMapOf(),
     var brothers: MutableMap<String, Double> = mutableMapOf()
@@ -44,7 +44,22 @@ data class ExtendedLocation(
     private set
 
   /**
-   * Boost the [score] by a given parent.
+   * The score boosts structure.
+   */
+  private val boost: Boost = Boost()
+
+  /**
+   * The list of [entities] names.
+   */
+  private val entitiesNames: List<String> by lazy { this.entities.map { it.normName } }
+
+  /**
+   * A map of entities scores associated by name
+   */
+  private val entitiesScoresMap: Map<String, Double> = this.entities.associate { it.name to it.score }
+
+  /**
+   * Boost the [score] of this location by a given [parent] and vice versa.
    *
    * @param parent the extended location of a parent of [location]
    */
@@ -53,7 +68,37 @@ data class ExtendedLocation(
     require(parent.location.id in this.location.parentsIds) { "Invalid parent." }
 
     if (this.parentIsInfluential(parent.location)) {
-      TODO()
+
+      var locationScoreBoost = 0.0
+      var parentScoreBoost = 0.0
+      val entitiesInters: Set<String> = this.entitiesNames.intersect(parent.entitiesNames)
+
+      parent.entitiesNames.subtract(entitiesInters).forEach {
+
+        var boost: Double = parent.getEntityScore(it)
+
+        this.boost.parents[it] = boost
+
+        // If the entity already boosted this location as child, mediates the boosts
+        this.boost.children[it]?.let { childBoost -> boost = (boost + childBoost) / 2 }
+
+        locationScoreBoost = max(locationScoreBoost, boost)
+      }
+
+      this.entitiesNames.subtract(entitiesInters).forEach {
+
+        var boost: Double = this.getEntityScore(it)
+
+        parent.boost.children[it] = boost
+
+        // If the entity already boosted the parent as parent, mediates the boosts
+        parent.boost.parents[it]?.let { parentBoost -> boost = (boost + parentBoost) / 2 }
+
+        parentScoreBoost = max(parentScoreBoost, boost)
+      }
+
+      this.score += locationScoreBoost
+      parent.score += parentScoreBoost / 2
     }
   }
 
@@ -66,4 +111,11 @@ data class ExtendedLocation(
     parent.type == Location.Type.AdminArea1 ||
       parent.type == Location.Type.AdminArea2 ||
       (parent.type == Location.Type.Country && !this.location.isInsideAdminArea2)
+
+  /**
+   * @param name the name of an entity that originated this location
+   *
+   * @return the score associated to the entity with the given [name]
+   */
+  private fun getEntityScore(name: String): Double = this.entitiesScoresMap.getValue(name)
 }
